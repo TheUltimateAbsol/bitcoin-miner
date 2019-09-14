d4905e4039a9e3c5b98a74888e39c5bbe51404c6
Craig <craigwstone@gmail.com>
2019-09-14 18:20:39 -0400

    More work on linking the pause menu

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,4 +1,4 @@
-extends Button
+extends Node2D
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -6,20 +6,12 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	if (is_pressed()):
-#		print("pressed")
 	pass # Replace with function body.
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-
-
 func _on_pause_button_pressed():
 	print("PAUSED")
 	get_tree().paused = true
-	
-	$pause_menu.draw()
+	$pause_menu.show()
