e1157086df57a403556c99d5477908dbfdde75ec
Craig <craigwstone@gmail.com>
2019-09-15 14:06:04 -0400

    Pause/Resume Functionality Implemented

--- Navigation/Scenes/Button.gd
+++ Navigation/Scenes/Button.gd
@@ -6,20 +6,12 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	if (is_pressed()):
-#		print("pressed")
 	pass # Replace with function body.
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 
-
-
-func _on_pause_button_pressed():
-	print("PAUSED")
-	get_tree().paused = true
-	
-	$pause_menu.draw()
+func _on_Button_pressed():
+	print("AAAA")
--- UI/Scenes/unpause_button.gd
+++ UI/Scenes/unpause_button.gd
@@ -5,5 +5,7 @@ signal unpause
 
 
 func _on_Button_pressed():
-	print("BUTTON");
 	emit_signal("unpause");
+
+
+
