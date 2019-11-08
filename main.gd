6eb2bbe6e02cea937db6049d4a47b199c9cb4cff
Brandon Ellis <thebellis618@gmail.com>
2019-11-08 01:00:35 -0500

    Added the ability to slide the VN

--- main.gd
+++ main.gd
@@ -1,9 +1,13 @@
-extends VSplitContainer
+extends Control
 
 onready var game = $PanelContainer/Panel/Node2D
 onready var game_container = $PanelContainer
 
+enum {VN, GAME, BOTH}
+var state = VN;
+
 func _ready():
+	to_Game();
 	pass
 #	update_size();
 
@@ -11,7 +15,30 @@ func update_size():
 	var margin_size = 40;
 	print(game);
 	print(game_container);
-	var scale = int(($PanelContainer.rect_size.y - margin_size)/144);
-	$PanelContainer/Panel/Node2D.stretch_shrink = scale;
-	$PanelContainer/Panel/Node2D.rect_min_size = scale*Vector2(160, 144);
-	$PanelContainer/Panel/Node2D.rect_size = scale*Vector2(160, 144);
+	var scale = int(($Game/PanelContainer.rect_size.y - margin_size)/144);
+	$Game/PanelContainer/Panel/Node2D.stretch_shrink = scale;
+	#$PanelContainer/Panel/Node2D.rect_min_size = scale*Vector2(160, 144);
+	$Game/PanelContainer/Panel/Node2D.rect_size = scale*Vector2(160, 144);
+	
+func to_VN():
+	if state == BOTH:
+		$AnimationPlayer.play("Both to VN");
+	if state == GAME:
+		$AnimationPlayer.play_backwards("VN to Game");
+	state = VN;
+		
+func to_Game():
+	if state == BOTH:
+		$AnimationPlayer.play("Both to Game");
+	if state == VN:
+		$AnimationPlayer.play("VN to Game");
+	state = GAME
+	
+func to_Both():
+	if state == VN:
+		$AnimationPlayer.play_backwards("Both to VN");
+	if state == GAME:
+		$AnimationPlayer.play_backwards("Both to Game");
+	state = BOTH
+	
+	
