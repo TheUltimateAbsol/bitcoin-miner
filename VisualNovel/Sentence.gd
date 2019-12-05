3eb17fc9a9cca132248a3a373faff0cfddc52a7d
Brandon Ellis <thebellis618@gmail.com>
2019-12-04 19:42:49 -0500

    Revert "Revert "Merge branch 'alana3' into brandon""
    This reverts commit 11067f3e50afa545339f5ee1499747ec3d260c30.

--- VisualNovel/Sentence.gd
+++ VisualNovel/Sentence.gd
@@ -3,8 +3,8 @@ extends Node
 class_name Sentence
 
 var content : String
-var sound : int
-var speed : float
+var sound
+var speed
 var delay : float
 
 func _init(xcontent, xdelay=0, xsound=Global.SoundEffect.NONE, xspeed=1):
