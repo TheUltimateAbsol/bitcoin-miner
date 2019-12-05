401c6b0489cd82fedb90491ddfd79bb477a3cdb9
Brandon Ellis <thebellis618@gmail.com>
2019-12-04 20:09:18 -0500

    Fixed changes that came from alana merges

--- main.gd
+++ main.gd
@@ -7,18 +7,18 @@ enum {VN, GAME, BOTH}
 var state = VN;
 
 func _ready():
-	to_Game(true);
+#	to_Game(true);
 	pass
 #	update_size();
 
 func update_size():
-#	var margin_size = 40;
-#	print(game);
-#	print(game_container);
-#	var scale = int(($Game/PanelContainer.rect_size.y - margin_size)/144);
-#	$Game/PanelContainer/Panel/Node2D.stretch_shrink = scale;
-#	#$PanelContainer/Panel/Node2D.rect_min_size = scale*Vector2(160, 144);
-#	$Game/PanelContainer/Panel/Node2D.rect_size = scale*Vector2(160, 144);
+	var margin_size = 40;
+	print(game);
+	print(game_container);
+	var scale = int(($Game/PanelContainer.rect_size.x - margin_size)/160);
+	$Game/PanelContainer/Panel/Node2D.stretch_shrink = scale;
+	#$PanelContainer/Panel/Node2D.rect_min_size = scale*Vector2(160, 144);
+	$Game/PanelContainer/Panel/Node2D.rect_size = scale*Vector2(160, 144);
 	pass
 	
 func to_VN(instant=false):
